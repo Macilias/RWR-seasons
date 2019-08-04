7fac324ced8a8e2db24c5b8355ec00ef2c6b4cf5
Maciek Niemczyk <Maciek.Niemczyk@vitruam.de>
2019-08-04 22:50:39 +0200

    refactors: 1. use componentDidMont 2. init state outside of constructor

--- src/index.js
+++ src/index.js
@@ -2,18 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
 
-		this.state = { lat: null, errMessage: '' };
+	state = { lat: null, errMessage: '' };
 
+  componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
-			(position) => {
-				this.setState({ lat: position.coords.latitude })
-			},
-			(err) => {
-				this.setState({ errMessage: err.message })
-			},
+			(position) => this.setState({ lat: position.coords.latitude }),
+			(err) => this.setState({ errMessage: err.message }),
 		);
 	}
 
